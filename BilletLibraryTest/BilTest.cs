dd4d5b1e3cf721f6a3701fa8a472976a0de2322c
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-09-17 22:48:21 +0200

    Adding testproject

---
+++ BilletLibraryTest/BilTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+
+namespace BilletLibraryTest
+{
+    [TestClass]
+    public class BilTest
+    {
+        [TestMethod]
+        public void BilPris()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            decimal pris = bil.Pris();
+            //Assert
+            Assert.AreEqual(240,pris);
+        }
+
+        [TestMethod]
+        public void KøretøjNavn()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            string pris = bil.Køretøj();
+            //Assert
+            Assert.AreEqual("Bil", pris);
+        }
+    }
+}
