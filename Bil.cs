b26ad07a4e6f580473c8941ce4ec3d5332447c4f
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-09-17 22:22:42 +0200

    Opgave 1

---
+++ Bil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BilletLibrary
+{
+    public class Bil
+    {
+        public string Nummerplade { get; set; }
+        public DateTime Dato { get; set; }
+    }
+}
