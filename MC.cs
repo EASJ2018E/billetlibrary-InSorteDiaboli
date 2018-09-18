9f9adc38ea4edc77c9c2f2388091090442d5cf42
Louis Verond Høgholt <louisvhoegholt@gmail.com>
2018-09-18 08:16:02 +0200

    D
    Commit

--- MC.cs
+++ MC.cs
@@ -5,34 +5,32 @@ using System.Text;
 namespace BilletLibrary
 {
     /// <summary>
-    /// Bilklasse som opretter et objekt af bil når det intialiseres
+    /// Bilklasse som opretter et objekt af mc når det intialiseres
     /// </summary>
     /// <returns></returns>
-    public class Bil
+    public class MC
     {
         public string Nummerplade { get; set; }
         public DateTime Dato { get; set; }
 
         /// <summary>
-        /// Metode til at printe prisen på bilen
+        /// Metode til at printe prisen på MC
         /// </summary>
         /// <returns></returns>
         public decimal Pris()
         {
-            int pris = 240;
+            int pris = 125;
             return pris;
         }
 
         /// <summary>
-        /// Metode til at printe køretøjets navn ud
+        /// Metode til at printe navnet på køretøjet
         /// </summary>
         /// <returns></returns>
         public string Køretøj()
         {
-            string bil = "Bil";
-            return bil;
+            string køretøj = "MC";
+            return køretøj;
         }
     }
-
-
 }
